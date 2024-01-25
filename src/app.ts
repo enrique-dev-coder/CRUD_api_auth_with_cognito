cc5ae9d3fc4315d3600f03904de5d81947a13dcd
eli castellano <eliman1926@gmail.com>
2024-01-25 17:26:18 -0400

    Add aws cognito

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import helmet from "helmet";
 import cors from "cors";
 import router from "./routes/task";
+import authRouter from "./routes/auth"
 
 const app: Express = express();
 
@@ -14,6 +15,8 @@ app.use(helmet());
 
 //routes
 app.use('/api', router)
+app.use('/auth', authRouter);
+
 
 app.get("/", (req: Request, res: Response) => {
   res.json("hola desde el server");
